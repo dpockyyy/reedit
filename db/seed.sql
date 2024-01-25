59c3fe5385d239ff08b444532b673588b9ed5833
dpark <davidparkus04@gmail.com>
2024-01-25 13:22:14 +1100

    adjusted margins of home page

--- db/seed.sql
+++ db/seed.sql
@@ -15,3 +15,13 @@ My question now is kind of a typical debate of quality v.s. quantity. More speci
 Any advice or suggestions on what I should do is greatly appreciated. General ideas on interview prep would also be super helpful.', 'zzzsteven30', 'leetcode', '2024-01-23 00:31:52');
 
 INSERT INTO posts (upvotes, title, image_url, description, username, subreedit, time) VALUES (75, 'Cool photo', 'https://i.pinimg.com/originals/68/62/c5/6862c57163be281ec95471fba21f87c7.jpg', 'taking photos are fun', 'James_Grapa7733', 'photos', '2024-01-21 12:31:52');
+
+INSERT INTO posts (upvotes, title, description, username, subreedit, time) VALUES (12, 'Is the grind75/neetcode75 plan still viable?', 'Getting back to job hunting and before I dive in I just want to make sure the path is still viable. Last time I did some of the grind 75 problems. I’m thinking about some DSnA review then jumping back in where I left off. Then continuing on to the expanded lists if I need to. I will of course be making sure to internalize the important concepts. Is that still a good plan for LC?
+
+For system design, is Alex Xu’s book a good place to start? I haven’t had a lot of sys design questions but I should expect them for a mid-level search.
+
+Thanks for answer and for any tips about the changing landscape over the last year or so.', 'Firm_Bit12', 'leetcode', '2024-01-13 12:31:52');
+
+INSERT INTO posts (upvotes, title, description, username, subreedit, time) VALUES (, '', '', '', '', '2024-01-21 12:31:52');
+
+INSERT INTO posts (upvotes, title, description, username, subreedit, time) VALUES (3, 'Im f***ed', 'Basically i have an amazon interview coming up in a few days and for the love of god, not a single question is striking to me. I am fucking up the coding, some easy qyestions i cant find the solution or even the approach. Should i just give up or keep trying pls help', 'Exact-Conclusion5793', 'leetcode', '2024-01-12 12:31:52');
